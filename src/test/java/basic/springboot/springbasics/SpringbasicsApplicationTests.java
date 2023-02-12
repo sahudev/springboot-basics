549af19804a178d1f4ebaa473cfcb15535a6aea0
Devendra Sahu <devendra.sahu19@gmail.com>
2023-02-12 09:00:09 +0530

    Assignment-1 (POST, DELETE, PATCH) 20230206 Feb 6th

---
+++ src/test/java/basic/springboot/springbasics/SpringbasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package basic.springboot.springbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
